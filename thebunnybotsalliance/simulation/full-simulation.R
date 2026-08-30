d9587e47088dc24cf0d60027d47258350db507a6
Ghostyaq <mitchellhung176@gmail.com>
2026-08-30 14:53:07 -0400

    added file to run the full simulation \ playoffs

---
+++ thebunnybotsalliance/simulation/full-simulation.R
@@ -0,0 +1,8 @@
+rm(list = ls())
+
+source("thebunnybotsalliance/simulation/creating-teams.R")
+source("thebunnybotsalliance/simulation/creating-schedule.R")
+source("thebunnybotsalliance/simulation/creating-matches.R")
+source("thebunnybotsalliance/simulation/creating-rankings.R")
+
+# Schedule is predetermined via the MatchMaker algorithm, found in matches.txt
